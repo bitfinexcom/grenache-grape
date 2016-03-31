16db1eaefc2fedf120be886a329b40a4c6bd650a
Paolo Ardoino <paolo.ardoino@gmail.com>
2016-03-31 18:35:36 +0100

    Initial commit

---
+++ examples/grape.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var Grape = require('./../lib/Grape')
+
+var g1 = new Grape({
+  dht_port: 20001,
+  dht_bootstrap: [
+    '127.0.0.1:20002'
+  ],
+  api_port: 30001
+})
+
+g1.start()
+
+var g2 = new Grape({
+  dht_port: 20002,
+  dht_bootstrap: [
+    '127.0.0.1:20001'
+  ],
+  api_port: 30002
+})
+
+g2.start()
