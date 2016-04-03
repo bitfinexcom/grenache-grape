6bbb7600e142373fcb8fd46ed2f04ad115ea62bb
Paolo Ardoino <paolo.ardoino@gmail.com>
2016-04-03 11:29:38 +0100

    bin script help

--- bin/grape.js
+++ bin/grape.js
@@ -9,13 +9,29 @@ var argv = minimist(process.argv.slice(2))
 
 if (!argv.dp) {
   console.error('Grape: no dht-port (--dp) specified')
-  process.exit(-1)
+  argv.help = 1
 }
 
 if (!argv.ap) {
   console.error('Grape: no api-port (--ap) specified')
+  argv.help = 1
+}
+
+if (argv.help) {
+  console.log(
+    '\nGRAPE help\n' +
+    '--dp <INT> : DHT listening port\n' +
+    '--bn <IP:PORT[,IP:PORT]>: DHT Bootstrap Nodes\n' +
+    '--ap <INT> : Grape API port\n' +
+    '\nExamples:\n' +
+    'grape --dp 20001 --ap 30001 --bn \'127.0.0.1:20002,127.0.0.1:20003\'\n' +
+    'grape --dp 20002 --ap 30002 --bn \'127.0.0.1:20001,127.0.0.1:20003\'\n' +
+    'grape --dp 20003 --ap 30003 --bn \'127.0.0.1:20001,127.0.0.1:20002\'\n' +
+    ''
+  )
   process.exit(-1)
 }
+
 
 var dht_port = argv.dp
 var api_port = argv.ap
