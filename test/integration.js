dcc5e1a08e1117fb40a96c1c803ebcfd81a4f04a
Robert Kowalski <rok@kowalski.gd>
2017-06-12 15:02:40 +0200

    tests: fix/add test for return error if no port given

--- test/integration.js
+++ test/integration.js
@@ -8,17 +8,15 @@ describe('Grape integration', () => {
   it('should emit a ready event', (done) => {
     const grape1 = new Grape({
       dht_port: 20002,
-      dht_bootstrap: [ '127.0.0.1:20001', '127.0.0.1:20003' ],
-      api_port: 40001,
-      api_port_http: 40002
+      dht_bootstrap: [ '127.0.0.1:20001' ],
+      api_port_http: 40001
     })
 
     grape1.start(() => {})
 
     const grape2 = new Grape({
       dht_port: 20001,
-      dht_bootstrap: [ '127.0.0.1:20002', '127.0.0.1:20003' ],
-      api_port: 30001,
+      dht_bootstrap: [ '127.0.0.1:20002' ],
       api_port_http: 30002
     })
 
