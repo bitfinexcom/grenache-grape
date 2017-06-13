1094432d4280e6267dfd485765d890dd2145bdc0
prdn <paolo.ardoino@gmail.com>
2017-06-13 08:57:13 +0000

    added concurrency

--- bin/grape.js
+++ bin/grape.js
@@ -16,6 +16,11 @@ const program = require('yargs')
     type: 'number',
     demand: true
   })
+  .option('dc', {
+    describe: 'DHT concurrency',
+    alias: 'dht_concurrency',
+    type: 'number'
+  })
   .option('apw', {
     describe: 'WebSocket api port',
     alias: 'api_port',
@@ -36,9 +41,9 @@ const program = require('yargs')
   })
   .help('help')
   .version()
-  .example('grape --dp 20001 --apw 30001 --aph 40001 --bn \'127.0.0.1:20002,127.0.0.1:20003\'')
-  .example('grape --dp 20002 --dh 127.0.0.1 --apw 30002 --aph 40002 --bn \'127.0.0.1:20001,127.0.0.1:20003\'')
-  .example('grape --dp 20003 --apw 30003 --aph 40003 --bn \'127.0.0.1:20001,127.0.0.1:20002\'')
+  .example('grape --dp 20001 --dc 32 --apw 30001 --aph 40001 --bn \'127.0.0.1:20002,127.0.0.1:20003\'')
+  .example('grape --dp 20002 --dc 32 --dh 127.0.0.1 --apw 30002 --aph 40002 --bn \'127.0.0.1:20001,127.0.0.1:20003\'')
+  .example('grape --dp 20003 --dc 32 --apw 30003 --aph 40003 --bn \'127.0.0.1:20001,127.0.0.1:20002\'')
   .usage('Usage: $0 --dp <val> --awp <val> --aph <val> --bn <val>')
   .argv
 
