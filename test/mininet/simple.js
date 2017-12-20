e06145f64e2b7f6b7331904b7a856ab3278094fc
Mathias Buus <mathiasbuus@gmail.com>
2017-12-20 23:02:19 +0100

    style

--- test/mininet/simple.js
+++ test/mininet/simple.js
@@ -1,14 +1,14 @@
-var mininet = require('mininet')
+const mininet = require('mininet')
 
-var mn = mininet()
+const mn = mininet()
 
-var s1 = mn.createSwitch()
+const s1 = mn.createSwitch()
 
-var bootstrap1 = mn.createHost()
-var bootstrap2 = mn.createHost()
+const bootstrap1 = mn.createHost()
+const bootstrap2 = mn.createHost()
 
-var c1 = mn.createHost()
-var w1 = mn.createHost()
+const c1 = mn.createHost()
+const w1 = mn.createHost()
 
 bootstrap1.link(s1)
 bootstrap2.link(s1)
@@ -27,7 +27,7 @@ mn.on('start', function () {
   bootstrap2.spawn('grape --dp 20000 --aph 30000 --bn ' + bootstrap1.ip + ':20000')
   setTimeout(function () {
     console.log('Starting workers and clients')
-    var bn = bootstrap1.ip + ':20000,' + bootstrap2.ip + ':20000'
+    const bn = bootstrap1.ip + ':20000,' + bootstrap2.ip + ':20000'
     c1.spawn('grape --dp 20000 ---aph 30000 --bn ' + bn + ' & node client.js')
     w1.spawn('grape --dp 20000 ---aph 30000 --bn ' + bn + ' & node worker.js')
   }, 1000)
