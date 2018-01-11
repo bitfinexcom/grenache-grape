4ba43dc444a0972070357028d4e6081c55616bf9
Robert Kowalski <rok@kowalski.gd>
2018-01-11 21:30:31 +0100

    test: scenario where 1 worker of 2 dies, same key
    in this scenario two workers have the same key, one dies and the
    other keeps announcing. additionally two clients keep doing
    lookups.

--- test/announce-removal-hotkey-two-worker.js
+++ test/announce-removal-hotkey-two-worker.js
@@ -5,7 +5,7 @@
 const { Grape } = require('./../')
 const assert = require('assert')
 
-describe('service announces - mixed lookups and killed worker', () => {
+describe('service announces - mixed lookups and 1 worker from 2 killed', () => {
   it('should remove outdated services after ~100ms', (done) => {
     const grape1 = new Grape({
       dht_port: 20002,
@@ -25,11 +25,16 @@ describe('service announces - mixed lookups and killed worker', () => {
     grape2.start(() => {})
 
     let ts
+    let interAnnounceSameKey
     grape1.on('ready', () => {
       // announce 1 time, then stop announcing to simulate crashed worker
       grape1.announce('rest:util:net', 1337, () => {
         ts = Date.now()
       })
+
+      interAnnounceSameKey = setInterval(() => {
+        grape1.announce('rest:util:net', 1338, () => {})
+      }, 25)
     })
 
     grape2.on('announce', () => {
@@ -44,7 +49,9 @@ describe('service announces - mixed lookups and killed worker', () => {
       inter = setInterval(() => {
         if (removed === 2 && !shutdown) {
           assert.equal(removed, 2, 'worker removed from both grapes')
+
           clearInterval(inter)
+          clearInterval(interAnnounceSameKey)
           grape1.stop(() => { grape2.stop(done) })
           shutdown = true
           return
@@ -52,23 +59,27 @@ describe('service announces - mixed lookups and killed worker', () => {
 
         grape1.lookup('rest:util:net', (_, res) => {
           console.log('grape1:', res, 'ms:', Date.now() - ts)
-          if (!res.length) {
-            removed++
+          if (res.length === 2) {
+            assert.deepEqual(res, [ '127.0.0.1:1337', '127.0.0.1:1338' ])
             return
           }
 
-          assert.deepEqual(res, [ '127.0.0.1:1337' ])
+          if (res.length === 1) {
+            removed++
+          }
         })
         grape2.lookup('rest:util:net', (_, res) => {
           console.log('grape2:', res, 'ms:', Date.now() - ts)
-          if (!res.length) {
-            removed++
+          if (res.length === 2) {
+            assert.deepEqual(res, [ '127.0.0.1:1337', '127.0.0.1:1338' ])
             return
           }
 
-          assert.deepEqual(res, [ '127.0.0.1:1337' ])
+          if (res.length === 1) {
+            removed++
+          }
         })
-      }, 8)
+      }, 10)
     }
   }).timeout(5000)
 })
