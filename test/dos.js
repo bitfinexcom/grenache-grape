ba25709e8c5ca6ccf1e2ca48ec1cbd5eb6cd9ed0
Robert Kowalski <rok@kowalski.gd>
2018-04-16 16:37:35 +0200

    dos: do not crash on invalid json

--- test/dos.js
+++ test/dos.js
@@ -36,4 +36,28 @@ describe('dos vector', () => {
       }
     })
   }).timeout(10000)
+
+  it('does not crash on invalid payloads', (done) => {
+    const {grape1, grape2, stop} = createTwoGrapes()
+
+    grape1.on('ready', () => {
+      grape1.announce('rest:util:net', 1337, () => {})
+    })
+
+    grape2.on('announce', () => {
+      const req = request({
+        uri: 'http://127.0.0.1:40001',
+        method: 'POST'
+      }).on('data', (data) => {
+        assert.equal(data.toString(), 'ERR_GRAPE_PAYLOAD_INVALID')
+        stop(done)
+      })
+      const writer = new PassThrough()
+      writer.pipe(req)
+
+      writer.push('garbage')
+      writer.end('')
+
+    })
+  }).timeout(5000)
 })
