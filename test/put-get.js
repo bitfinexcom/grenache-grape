f2dadfd138e2527676f1955472dc28198479d97a
David Mark Clements <huperekchuno@googlemail.com>
2019-08-12 17:23:17 +0200

    put-get test refactor

--- test/put-get.js
+++ test/put-get.js
@@ -4,77 +4,67 @@
 
 const assert = require('assert')
 const request = require('request')
+const once = require('events.once')
+const { promisify } = require('util')
 
 const {
   createTwoGrapes
 } = require('./helper.js')
 
-function getValue (h, cb) {
-  request.post({
+const post = promisify(request.post)
+
+async function getValue (h) {
+  const { body } = await post({
     uri: 'http://127.0.0.1:40001/get',
     json: true,
     body: { rid: 'test', data: h }
-  }, (err, res, body) => {
-    if (err) return cb(err)
-
-    cb(null, body)
   })
+  return body
 }
 
 describe('put-get', () => {
-  it('works with hashes', (done) => {
+  it('works with hashes', async () => {
     const { grape1, grape2, stop } = createTwoGrapes()
+    await once(grape1, 'ready')
+    grape1.announce('rest:util:net', 1337, () => {})
+    await once(grape2, 'announce')
+    const data = {
+      'v': 'hello world'
+    }
+    const { body: hash } = await post({
+      uri: 'http://127.0.0.1:40001/put',
+      json: true,
+      body: { rid: 'test', data: data }    })
 
-    grape1.on('ready', () => {
-      grape1.announce('rest:util:net', 1337, () => {})
-    })
+    const normative = await getValue(hash)
+    assert.strictEqual(typeof normative.id, 'string')
+    assert.strictEqual(normative.id.length, 64)
+    assert.strictEqual(typeof normative.k, 'string')
+    assert.strictEqual(normative.k.length, 64)
+    assert.strictEqual(normative.v, 'hello world')
+    assert.strictEqual(normative.m, false)
+    assert.strictEqual(normative.seq, null)
 
-    grape2.on('announce', () => {
-      const data = {
-        'v': 'hello world'
-      }
+    const explicit = await getValue({ hash, m: false })
+    assert.strictEqual(typeof explicit.id, 'string')
+    assert.strictEqual(explicit.id.length, 64)
+    assert.strictEqual(typeof explicit.k, 'string')
+    assert.strictEqual(explicit.k.length, 64)
+    assert.strictEqual(explicit.v, 'hello world')
+    assert.strictEqual(explicit.m, false)
+    assert.strictEqual(explicit.seq, null)
+    assert.strictEqual(explicit.sig, null)
 
-      request.post({
-        uri: 'http://127.0.0.1:40001/put',
-        json: true,
-        body: { rid: 'test', data: data }
-      }, (err, res, hash) => {
-        if (err) throw err
-        getValue(hash, (err, res) => {
-          if (err) throw err
-          assert.strictEqual(typeof res.id, 'string')
-          assert.strictEqual(res.id.length, 64)
-          assert.strictEqual(typeof res.k, 'string')
-          assert.strictEqual(res.k.length, 64)
-          assert.strictEqual(res.v, 'hello world')
-          assert.strictEqual(res.m, false)
-          assert.strictEqual(res.seq, null)
-          assert.strictEqual(res.sig, null)
-          getValue({ hash: hash, m: false }, (err, res) => {
-            if (err) throw err
-            assert.strictEqual(typeof res.id, 'string')
-            assert.strictEqual(res.id.length, 64)
-            assert.strictEqual(typeof res.k, 'string')
-            assert.strictEqual(res.k.length, 64)
-            assert.strictEqual(res.v, 'hello world')
-            assert.strictEqual(res.m, false)
-            assert.strictEqual(res.seq, null)
-            assert.strictEqual(res.sig, null)
-            getValue({ hash: hash }, (err, res) => {
-              if (err) throw err
-              assert.strictEqual(typeof res.id, 'string')
-              assert.strictEqual(res.id.length, 64)
-              assert.strictEqual(typeof res.k, 'string')
-              assert.strictEqual(res.k.length, 64)
-              assert.strictEqual(res.v, 'hello world')
-              assert.strictEqual(res.m, false)
-              assert.strictEqual(res.seq, null)
-              assert.strictEqual(res.sig, null)
-              stop(done)
-            })
-          })
-        })
-      })
-    })
+    const legacy = await getValue({ hash })
+    assert.strictEqual(typeof legacy.id, 'string')
+    assert.strictEqual(legacy.id.length, 64)
+    assert.strictEqual(typeof legacy.k, 'string')
+    assert.strictEqual(legacy.k.length, 64)
+    assert.strictEqual(legacy.v, 'hello world')
+    assert.strictEqual(legacy.m, false)
+    assert.strictEqual(legacy.seq, null)
+    assert.strictEqual(legacy.sig, null)
+
+    stop()
   }).timeout(5000)
 })
