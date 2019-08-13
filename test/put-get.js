2d08b8c97e913bdf4ebdc7d6fceee67338e17735
David Mark Clements <huperekchuno@googlemail.com>
2019-08-14 01:56:02 +0200

    salt support

--- test/put-get.js
+++ test/put-get.js
@@ -97,7 +97,7 @@ describe('put-get', () => {
 
     assert.strictEqual(hexKey, key.toString('hex'))
 
-    const normative = await getValue({ key: hexKey })
+    const normative = await getValue({key: hexKey})
     assert.strictEqual(typeof normative.id, 'string')
     assert.strictEqual(normative.id.length, 64)
     assert.strictEqual(normative.k, hexKey)
@@ -130,4 +130,70 @@ describe('put-get', () => {
 
     stop()
   }).timeout(5000)
+
+  it('stores and retrieves salted mutable data', async () => {
+    const { grape1, grape2, stop } = createTwoGrapes()
+    await once(grape1, 'ready')
+    grape1.announce('rest:util:net', 1337, () => {})
+    await once(grape2, 'announce')
+    const keypair = hypersign.keypair()
+    const salt = 'test salt'
+    const { publicKey: key } = keypair
+    const value = 'hello world'
+    const sig = hypersign.sign(Buffer.from(value), { 
+      keypair, 
+      salt: hypersign.salt(salt)
+    })
+
+    const data = {
+      v: value,
+      k: key,
+      sig,
+      salt
+    }
+
+    const { body: hexKey } = await post({
+      uri: 'http://127.0.0.1:40001/put',
+      json: true,
+      body: { rid: 'test', data: data }
+    })
+
+    assert.strictEqual(hexKey, key.toString('hex'))
+
+    const normative = await getValue({key: hexKey, salt})
+
+    assert.strictEqual(typeof normative.id, 'string')
+    assert.strictEqual(normative.id.length, 64)
+    assert.strictEqual(normative.k, hexKey)
+    assert.strictEqual(normative.v, 'hello world')
+    assert.strictEqual(normative.m, true)
+    assert.strictEqual(normative.seq, 0)
+    assert.strictEqual(normative.sig, sig.toString('hex'))
+    assert.strictEqual(normative.salt, hypersign.salt(salt).toString('hex'))
+
+    const explicit = await getValue({ hash: hexKey, m: true, salt })
+    assert.strictEqual(typeof explicit.id, 'string')
+    assert.strictEqual(explicit.id.length, 64)
+    assert.strictEqual(explicit.k, hexKey)
+    assert.strictEqual(explicit.v, 'hello world')
+    assert.strictEqual(explicit.m, true)
+    assert.strictEqual(explicit.seq, 0)
+    assert.strictEqual(explicit.sig, sig.toString('hex'))
+    assert.strictEqual(explicit.salt, hypersign.salt(salt).toString('hex'))
+
+    const legacy = await getValue({ hash: hexKey, salt })
+    assert.strictEqual(typeof legacy.id, 'string')
+    assert.strictEqual(legacy.id.length, 64)
+    assert.strictEqual(typeof legacy.k, 'string')
+    assert.strictEqual(legacy.k.length, 64)
+    assert.strictEqual(legacy.v, 'hello world')
+    assert.strictEqual(legacy.m, true)
+    assert.strictEqual(legacy.seq, 0)
+    assert.strictEqual(legacy.sig, sig.toString('hex'))
+    assert.strictEqual(legacy.salt, hypersign.salt(salt).toString('hex'))
+
+    stop()
+  }).timeout(5000)
+
+
 })
