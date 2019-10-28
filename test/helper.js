efca276efe296bdefd928b8a1a3dc53140ad40c0
David Mark Clements <huperekchuno@googlemail.com>
2019-10-28 16:06:08 +0100

    test setup improvements

--- test/helper.js
+++ test/helper.js
@@ -10,13 +10,23 @@ async function createGrapes (n, onstart = () => {}) {
   const bootstrap = new Grape({
     dht_port: bsPort,
     dht_bootstrap: false,
-    api_port: await getPort(),
+    api_port: await getPort(bsPort + 1),
     dht_peer_maxAge: 200
   })
   bootstrap.start()
   await once(bootstrap, 'ready')
-  const dhtPorts = await Promise.all([...Array(n)].map(() => getPort()))
-  const apiPorts = await Promise.all([...Array(n)].map(() => getPort()))
+
+  const ports = async (n, offset = 0) => {
+    const result = new Array(n)
+    result[0] = await getPort(offset)
+    for (var i = 1; i < n; i++) {
+      result[i] = await getPort(result[i - 1] + 1 + offset)
+    }
+    return result
+  }
+
+  const dhtPorts = await ports(n)
+  const apiPorts = await ports(n, n + 1)
   for (const [i, port] of dhtPorts.entries()) {
     const grape = new Grape({
       dht_port: port,
